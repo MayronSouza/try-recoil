dd6022ee7b82a2ae7648412f797573c50924e994
MayronSouza <mayron.souza@gmz.solutions>
2020-12-30 17:18:11 -0300

    Utilizando o selector do recoil para modificar o estado do atom sem que seja preciso recarregar a page

---
+++ src/atoms/selector/index.js
@@ -0,0 +1,13 @@
+import { selector } from 'recoil';
+
+import usernameState from '../../atoms';
+
+export const upperUsername = selector({
+  key: 'upperUsernameState',
+  get: ({ get }) => {
+    const username = get(usernameState);
+    const usernameUpper = username.toUpperCase();
+
+    return usernameUpper;
+  }
+});
